36c7a043f9f457b888dabd76744a215173f6a32a
Lucas ADAWI <ladawi@e2r6p20.clusters.42paris.fr>
2022-04-02 20:29:46 +0200

    RATIO

---
+++ Module02/ex02/Fixed.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ladawi <ladawi@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 17:58:33 by ladawi            #+#    #+#             */
+/*   Updated: 2022/04/02 20:23:18 by ladawi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+#ifndef FIXED_H
+# define FIXED_H
+
+class Fixed {
+
+public:
+
+	Fixed(void);
+	Fixed(int const inb);
+	Fixed(float const fnb);
+	Fixed(Fixed const & src);
+	~Fixed(void);
+
+	Fixed &		operator=(Fixed const & rhs);
+
+	int		getRawBits(void) const;
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
+private:
+
+	int		_nb;
+	static int const _comma;
+
+};
+
+std::ostream &	operator<<(std::ostream & o, Fixed const & rhs);
+
+#endif
